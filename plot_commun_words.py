9eaebaad68acf02afe9cf20fbab7f9e2666f77ae
ykcir <ramoncayomar@unal.edu.co>
2020-03-12 10:35:52 -0500

    funcion plot

---
+++ plot_commun_words.py
@@ -0,0 +1,24 @@
+def plot_common_words(count_data, count_vectorizer,nWordsMin,nWordsMax):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import numpy as np
+    sns.set_style('whitegrid')
+    words = count_vectorizer.get_feature_names()
+    total_counts = np.zeros(len(words))
+    for t in count_data:
+        total_counts += t.toarray()[0]
+
+    count_dict = (zip(words, total_counts))
+    count_dict = sorted(count_dict, key=lambda x: x[1], reverse=True)[nWordsMin:nWordsMax]
+    words = [w[0] for w in count_dict]
+    counts = [w[1] for w in count_dict]
+    x_pos = np.arange(len(words))
+
+    plt.figure(2, figsize=(15, 15 / 1.6180))
+    plt.subplot(title='10 most common words')
+    sns.set_context("notebook", font_scale=1.25, rc={"lines.linewidth": 2.5})
+    sns.barplot(x_pos, counts, palette='husl')
+    plt.xticks(x_pos, words, rotation=90)
+    plt.xlabel('words')
+    plt.ylabel('counts')
+    plt.show()
